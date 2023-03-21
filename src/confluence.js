40f7cb83835335955d0f764648b4e961d275863f
Mazey Chu <mazey@mazey.net>
2023-03-22 00:08:51 +0800

    feat: Record the log
    Record the visitor log of CF.
    
    Close #19

--- src/confluence.js
+++ src/confluence.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-undef */
+import { formatDate, getCookie } from 'mazey';
+
 console.log('confluence');
 
 // Area: `.wiki-content`
@@ -7,4 +9,21 @@ if (window.$) {
   $('.wiki-content>p>br').hide();
   // Table
   $('.wiki-content td.confluenceTd>a').after('<br />');
+  // Record
+  const userName = getCookie('username') || 'unknown';
+  const settings = {
+    url: 'https://i.mazey.net/server/log/add',
+    method: 'POST',
+    timeout: 0,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify({
+      log_type: 'confluence',
+      content: `「${userName}」visited「[${document.title}](${location.href})」at「${formatDate(new Date(), 'yyyy-MM-dd hh:mm:ss')}」`,
+    }),
+  };
+  $.ajax(settings).done(function (response) {
+    // console.log(response);
+  });
 }
