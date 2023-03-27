c3c4d67a3de5fb84ffef4458e024d2d787678c45
Mazey Chu <mazey@mazey.net>
2023-03-27 11:36:29 +0800

    feat: remove Hide sidebar and Hide Header when TOC

--- src/pages/wordpress/index.js
+++ src/pages/wordpress/index.js
@@ -1,27 +1,26 @@
 import { loadScript, isSafePWAEnv } from 'mazey';
 import { hideHeaderInTOC, hideSidebar } from 'mazey-wordpress-utils';
 import { Nav, Style, Container, Address, setLocal } from './previous';
-import './index.scss';
 
 (function (window) {
-  setLocal('polestar_version', 'Saturday_May_21st_2022');
+  setLocal('polestar_version', 'Monday_March_27th_2023');
   window.polestarLoadScript = loadScript;
   window.polestarIsSafePWAEnv = isSafePWAEnv;
   window.hideHeaderInTOC = hideHeaderInTOC;
   window.hideSidebar = hideSidebar;
   // Hide sidebar
-  hideSidebar({
-    urlContainList: [
-      'hide_sidebar',
-      '/2740.html', // Don't fly too low. Be the best version of yourself.
-    ],
-  });
+  // hideSidebar({
+  //   urlContainList: [
+  //     'hide_sidebar',
+  //     '/2740.html', // Don't fly too low. Be the best version of yourself.
+  //   ],
+  // });
   // Hide Header when TOC
-  hideHeaderInTOC({
-    urlContainList: [
-      '1878.html', // Postman 使用小技巧/指南
-    ],
-  });
+  // hideHeaderInTOC({
+  //   urlContainList: [
+  //     '1878.html', // Postman 使用小技巧/指南
+  //   ],
+  // });
   Nav.addBlank();
   Style.alterSearch();
   Container.hideAdminHeader();
