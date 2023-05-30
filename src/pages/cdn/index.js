2aad6ca212036c3f7332fd3a5dd9436ca651326f
Mazey Chu <mazey@mazey.net>
2023-05-30 15:53:48 +0800

    feat: clear the not_found file

--- src/pages/cdn/index.js
+++ src/pages/cdn/index.js
@@ -1,14 +1,15 @@
+/**
+ * Load resources from a CDN.
+ */
 import { loadCSS, loadScript } from 'mazey';
 
-loadCSS('//i.mazey.net/cdn/fontawesome/5.15.4/css/all.css');
-loadCSS('//i.mazey.net/cdn/bootstrap/3.4.1/css/bootstrap.min.css');
-loadCSS('//i.mazey.net/mazey-style/lib/confluence.css');
-loadScript('//i.mazey.net/cdn/jquery/2.1.1/jquery.min.js');
-loadScript('//i.mazey.net/cdn/lodash/4.17.21/lodash.min.js');
-loadScript('//i.mazey.net/cdn/vue/2.6.12/vue.min.js');
-loadScript('//i.mazey.net/cdn/bootstrap/3.4.1/js/bootstrap.min.js');
-loadScript('//i.mazey.net/cdn/layer/layer.js');
-// loadScript('//i.mazey.net/cdn/layer/mobile/layer.js');
-loadScript('//i.mazey.net/cdn/html5shiv/3.7.3/html5shiv.min.js');
-loadScript('//i.mazey.net/cdn/respond/1.4.2/respond.min.js');
-loadScript('//i.mazey.net/mazey/lib/mazey.min.js');
+loadCSS('//i.mazey.net/lib/fontawesome/5.15.4/css/all.css');
+loadCSS('//i.mazey.net/lib/bootstrap/3.4.1/css/bootstrap.min.css');
+loadScript('//i.mazey.net/lib/jquery/2.1.1/jquery.min.js');
+loadScript('//i.mazey.net/lib/lodash/4.17.21/lodash.min.js');
+loadScript('//i.mazey.net/lib/vue/2.6.12/vue.min.js');
+loadScript('//i.mazey.net/lib/bootstrap/3.4.1/js/bootstrap.min.js');
+loadScript('//i.mazey.net/lib/layer/layer.js');
+loadScript('//i.mazey.net/lib/html5shiv/3.7.3/html5shiv.min.js');
+loadScript('//i.mazey.net/lib/respond/1.4.2/respond.min.js');
+loadScript('//i.mazey.net/lib/mazey/3.6.6/mazey.min.js');
