8f32b153b68c710386f3fbb36f9be1041c7a6f41
Mazey Chu <mazey@mazey.net>
2023-05-30 16:00:16 +0800

    fix: Require jQuery

--- src/pages/cdn/index.js
+++ src/pages/cdn/index.js
@@ -5,11 +5,14 @@ import { loadCSS, loadScript } from 'mazey';
 
 loadCSS('//i.mazey.net/lib/fontawesome/5.15.4/css/all.css');
 loadCSS('//i.mazey.net/lib/bootstrap/3.4.1/css/bootstrap.min.css');
-loadScript('//i.mazey.net/lib/jquery/2.1.1/jquery.min.js');
+// Require jQuery
+loadScript('//i.mazey.net/lib/jquery/2.1.1/jquery.min.js')
+  .then(() => {
+    loadScript('//i.mazey.net/lib/bootstrap/3.4.1/js/bootstrap.min.js');
+    loadScript('//i.mazey.net/lib/layer/layer.js');
+  });
 loadScript('//i.mazey.net/lib/lodash/4.17.21/lodash.min.js');
 loadScript('//i.mazey.net/lib/vue/2.6.12/vue.min.js');
-loadScript('//i.mazey.net/lib/bootstrap/3.4.1/js/bootstrap.min.js');
-loadScript('//i.mazey.net/lib/layer/layer.js');
 loadScript('//i.mazey.net/lib/html5shiv/3.7.3/html5shiv.min.js');
 loadScript('//i.mazey.net/lib/respond/1.4.2/respond.min.js');
 loadScript('//i.mazey.net/lib/mazey/3.6.6/mazey.min.js');
